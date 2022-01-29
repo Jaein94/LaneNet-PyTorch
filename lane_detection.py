d6773d296ca94f9654ea3645758834ad39108510
JRS <jt1640@naver.com>
2022-01-30 01:44:44 +0900

    플젝 영상 바꾸고 최적화

--- lane_detection.py
+++ lane_detection.py
@@ -13,13 +13,12 @@ from utils.evaluation import gray_to_rgb_emb, process_instance_embedding
 def region_of_interset(image):
     height = image.shape[0]
     polygon = np.array([
-        [(0,256),(110,256),(256,90),(200,90)],
-        [(512-110,256),(512,256),(300,90),(256,90)]
+        [(10,720),(1270,720),(732,460),(546,460)]
         ])
     mask = np.zeros_like(image)
     
     cv2.fillPoly(mask,polygon,(255,255,255))
-    # cv2.imshow('mask',mask)
+    cv2.imshow('mask',mask)
     # print(mask.shape)
     # print(type(mask))
     masked_image = cv2.bitwise_and(image,mask)
@@ -31,7 +30,9 @@ model_path = './TUSIMPLE/Lanenet_output/lanenet_epoch_39_batch_8_AUG.model'
 LaneNet_model = Lanenet(2, 4).to(device)
 LaneNet_model.load_state_dict(torch.load(model_path, map_location=device))
 
-video_file = '/home/amlab/Chungra_front_detect/701_front_cam/GH020042.MP4'
+print(device)
+# video_file = '/home/amlab/Chungra_front_detect/701_front_cam/GH020042.MP4'
+video_file = 'project_video.mp4'
 cap = cv2.VideoCapture(video_file)
 
 img_to_tensor = ToTensor()
@@ -42,23 +43,25 @@ if cap.isOpened():
         if ret:
 
             start = time.time()
-
-            x_min, x_max = 650, 970  #height
-            y_min, y_max = 600, 1400  #width
-            roi_img = img[x_min:x_max, y_min:y_max]
+            cv2.imshow('roi',img)
+            # print(img.shape)
+            # x_min, x_max = 546, 460  #height
+            # y_min, y_max = 732, 460  #width
+            # roi_img = img[x_min:x_max, y_min:y_max]
+            
 
             IMAGE_H = 256
             IMAGE_W = 512
-            roi_img = cv2.resize(roi_img, dsize=(IMAGE_W, IMAGE_H), interpolation=cv2.INTER_LINEAR)
+            roi_img = cv2.resize(img, dsize=(IMAGE_W, IMAGE_H), interpolation=cv2.INTER_LINEAR)
             
 
             # print(type(roi_img))
             # plt.imshow(roi_img)
             # roi_img = cv2.cvtColor(roi_img,cv2.COLOR_BGR2RGB)
             
-            dst = np.float32([[200, 80], [260, 80],[0, IMAGE_H] , [IMAGE_W, IMAGE_H]])
-            src = np.float32([[45, 0], [512-45, 0], [0, IMAGE_H], [IMAGE_W, IMAGE_H]])
-            M = cv2.getPerspectiveTransform(dst, src)
+            # dst = np.float32([[200, 80], [260, 80],[0, IMAGE_H] , [IMAGE_W, IMAGE_H]])
+            # src = np.float32([[45, 0], [512-45, 0], [0, IMAGE_H], [IMAGE_W, IMAGE_H]])
+            # M = cv2.getPerspectiveTransform(dst, src)
             # Minv = cv2.getPerspectiveTransform(dst, src)
 
             pre_process_roi_img = roi_img / 127.5 - 1.0
@@ -71,8 +74,11 @@ if cap.isOpened():
             # # image_show = gt_image_show.transpose(1,2,0)
             # # image_show = image_show[...,::-1]
             binary_img = torch.argmax(binary_final_logits, dim=1).squeeze().cpu().numpy()
+            print(binary_img.shape)
+            binary_img = cv2.resize(binary_img,(1280,720),interpolation=cv2.INTER_NEAREST)
+            print(binary_img.shape)
 
-            canvas = np.zeros((256,512,3))
+            canvas = np.zeros((720,1280,3))
             canvas[np.where(binary_img > 0)] = [255,255,255]
 
             masked_image = region_of_interset(canvas)
@@ -89,7 +95,7 @@ if cap.isOpened():
             #         # 검출된 선 그리기 ---③
             #         x1, y1, x2, y2 = line[0]
             #         cv2.line(roi_img, (x1,y1), (x2, y2), (0,255,0), 1)
-            cv2.imshow('roi',roi_img)
+            
 
             # cv2.imshow('pre',dst)
 
